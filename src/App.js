bb7c4c566e93036beaf6de4100aee1e6d3e85d2b
frangu617 <142462584+frangu617@users.noreply.github.com>
2024-01-08 21:02:30 -0800

    what was done in class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState} from 'react';
 
 function App() {
+  const [btnColor, setBtnColor] = useState('blue')
+  const newBtnColor = btnColor === 'green' ? 'blue' : 'green'
+  const [inputDisabled, setInputDisabled] = useState(false)
+  const [inputValue, setInputValue] = useState('Enabled')
+  const newInputValue =inputValue === 'Enabled' ? 'Disabled' : 'Enabled'
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing Playground</h1>
+      <p>Button is {inputValue}</p>
+      <button
+      style={{backgroundColor: btnColor}}
+      onClick= {() => setBtnColor(newBtnColor)}
+      disabled={inputDisabled}
+      >
+        Change to {newBtnColor}
+        </button>
+        <input
+        type="checkbox"
+        defaultChecked={inputDisabled} {inputValue}
+        onChange = {(event) => setInputDisabled(event.target.checked)}
+        />
     </div>
   );
 }
